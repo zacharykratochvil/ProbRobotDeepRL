fixed negative diameter calculation for goal ball

ppo_agent.py
@@ -61,16 +61,16 @@ class PPOAgent(nn.Module):
             # current_step = self.rewards[step][0].item()
         
 
-            print('as just number', self.rewards[step][0].item())
-            print('*'*30)
-            print('at each step',self.rewards[step])
-            print('*'*30)
+            #print('as just number', self.rewards[step][0].item())
+            #print('*'*30)
+            #print('at each step',self.rewards[step])
+            #print('*'*30)
             next_obs, next_done = torch.Tensor(next_obs).to(self.device), torch.Tensor(done).to(self.device)
             self.next_obs = next_obs
             self.next_done = next_done
-            print('*'*30)
-            print('whole tensor',self.rewards)
-            print('*'*30)
+            #print('*'*30)
+            #print('whole tensor',self.rewards)
+            #print('*'*30)
             if done == True:
                 break
 